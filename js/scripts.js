5c1fa96aa893560fe620e681fb7387ab873854ff
Tom Henderson <tomhenderson@mac.com>
2014-08-19 09:12:55 +1200

    Add no conflict wrapper.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+jQuery(document).ready(function($){
 	// Put spans around H2 elements tu cut out background
 	$('h2').html(function(){
 		var title = "<span>" + $(this).html() + "</span>";
