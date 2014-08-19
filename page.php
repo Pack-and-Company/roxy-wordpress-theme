da297896e00bb462588aa423a2370e9bb33877f8
Tom Henderson <tomhenderson@mac.com>
2014-08-19 17:03:17 +1200

    Check is user has already entered a $ sign for event price.

--- page.php
+++ page.php
@@ -37,7 +37,9 @@
                 foreach ( $events as $event ) {
                     $door_charge = get_post_meta($event->ID, '_event_price', true);
                     if ( $door_charge != '') {
-                        $door_charge = "$" . $door_charge;
+                    	if ( substr($door_charge, 0, 1) != "$" ) {
+	                        $door_charge = "$" . $door_charge;
+	                    }
                     }
 
                     $event_time = get_post_meta($event->ID, '_event_time', true);
