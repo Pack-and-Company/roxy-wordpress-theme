738fb3e6e7fe00cf6d33fdc82a5ffc43ba05f47d
Tom Henderson <tomhenderson@mac.com>
2014-09-07 14:36:31 +1200

    Added styles for mobile layout.

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
     ?>
   </title>
 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+
   <link type="text/css" rel="stylesheet" media="all" href="<?=get_template_directory_uri();?>/css/reset.css" />
   <link type="text/css" rel="stylesheet" media="all" href="<?=get_template_directory_uri();?>/css/fonts.css" />
   <link type="text/css" rel="stylesheet" media="all" href="<?=get_template_directory_uri();?>/css/prettyPhoto.css" />
